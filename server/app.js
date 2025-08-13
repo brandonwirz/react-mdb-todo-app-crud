1edfd696061caf732f1bb52fef98ef4e1b74c73d
brandonwirz <bwirz75@gmail.com>
2025-08-13 13:51:22 -0600

    updated server.js then, catch, error handler

--- server/app.js
+++ server/app.js
@@ -6,14 +6,24 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-
 app.use("/todos", todos)
-connectDB()
 
 app.get("/", (req, res) => {
-  res.send("Hello World......!!!")
+  res.send("Hello World......")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err)
+  })
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send("Something went wrong!")
 })
